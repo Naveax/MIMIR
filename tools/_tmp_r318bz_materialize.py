fbed3f0886cb8abad4a43b192221df89bc05a75f
Naveax <Omersevik095@gmail.com>
2026-09-14 16:36:28 +0300

    Separate R3.18BZ production and continuity authorities

--- tools/_tmp_r318bz_materialize.py
+++ tools/_tmp_r318bz_materialize.py
@@ -4,18 +4,21 @@ from pathlib import Path
 import argparse
 
 TOKENS = {
+    '__BASE_SHA__': 'base_sha',
+    '__BASE_TREE__': 'base_tree',
     '__BY_PUBLISHED_SHA__': 'by_published_sha',
     '__BY_PUBLISHED_TREE__': 'by_published_tree',
+    '__BY_CANDIDATE_SHA__': 'by_candidate_sha',
     '__BY_LIB_BLOB__': 'by_lib_blob',
     '__BY_TEST_BLOB__': 'by_test_blob',
     '__BY_PR_CI_RUN__': 'by_pr_ci_run',
     '__BY_PR_CI_JOB__': 'by_pr_ci_job',
-    '__BY_PR_KNOWLEDGE_RUN__': 'by_pr_knowledge_run',
-    '__BY_PR_KNOWLEDGE_JOB__': 'by_pr_knowledge_job',
     '__BY_MAIN_CI_RUN__': 'by_main_ci_run',
     '__BY_MAIN_CI_JOB__': 'by_main_ci_job',
-    '__BY_MAIN_KNOWLEDGE_RUN__': 'by_main_knowledge_run',
-    '__BY_MAIN_KNOWLEDGE_JOB__': 'by_main_knowledge_job',
+    '__BASE_MAIN_CI_RUN__': 'base_main_ci_run',
+    '__BASE_MAIN_CI_JOB__': 'base_main_ci_job',
+    '__BASE_MAIN_KNOWLEDGE_RUN__': 'base_main_knowledge_run',
+    '__BASE_MAIN_KNOWLEDGE_JOB__': 'base_main_knowledge_job',
 }
 
 parser = argparse.ArgumentParser()
